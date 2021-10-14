3cfd35da0b8a49753e7514cb32a9d4b2fed08c00
Croazt <fachry.croazt@gmail.com>
2021-10-14 14:15:43 +0700

    Done

--- app/src/main/java/com/psi/toserbalist/models/ApiService.kt
+++ app/src/main/java/com/psi/toserbalist/models/ApiService.kt
@@ -6,5 +6,5 @@ import retrofit2.http.*
 interface ApiService {
      @GET("Barang")
 
-    fun fetchMakanan(@Query("select") select : String,@Query("Category",) kategori : String): Call<List<BarangLists>>
+    fun fetchMakanan(@Query("select") select : String,@Query("Category") kategori : String): Call<List<BarangLists>>
 }
